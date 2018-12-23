b6b415a25d01991004c2df7b6cbd27561ad6b696
putrisiwi146 <44189148+putrisiwi146@users.noreply.github.com>
2018-12-24 00:09:45 +0700

    Add files via upload

---
+++ index.py
@@ -0,0 +1,16 @@
+print '<!DOCTYPE html>'
+print
+print '''<html>
+<head> <title> DATA DIRI </title> </head>'''
+
+print '''<body> <table> <tr> 
+	<td> <img src='PicsArt_11-08-06.57.14.jpg' width='350 px' height='450'> </img> </td>
+	<td> <h1> DATA DIRI </h1> '''
+
+print 'Nama: Putri Siwi Utami </br>'
+print 'Alamat tinggal: Jalan Duwet 12 </br>'
+print 'Tempat, tanggal lahir: Brebes,2 Januari 2001 </br>'
+print 'Tempat wisata favorit: Pantai </br>'
+print '''motto: Jangan pantang menyerah
+	</td> </tr> </table>'''
+print '</body> </html>'
